fae4a734fe256c41802d499770423844680fc0d3
cory <cory@ed5197a5-7fde-0310-b194-c3ffbd925b24>
2016-01-24 15:05:02 +0000

    Made a minimal todo list app.

---
+++ packages/todo/todo.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var kUnchecked = "☐";
+var kChecked = "☒";
+
+function readList(key) {
+	return imports.database.get(key).catch(function(error) {
+		return null;
+	}).then(function(todo) {
+		if (!todo) {
+			todo = JSON.stringify({name: "TODO", items: []});
+		}
+		return JSON.parse(todo);
+	});
+}
+
+function writeList(key, todo) {
+	return imports.database.set(key, JSON.stringify(todo));
+}
+
+imports.core.register("onInput", function(command) {
+	if (command.substring(0, "action:".length) == "action:") {
+		command = JSON.parse(command.substring("action:".length));
+		if (command.action == "set") {
+			setItem(command.key, command.item, command.value).then(redisplay);
+		} else if (command.action == "remove") {
+			removeItem(command.key, command.item).then(redisplay);
+		}
+	} else {
+		addItem("todo", command).then(redisplay);
+	}
+});
+
+function addItem(key, name) {
+	return readList(key).then(function(todo) {
+		todo.items.push({name: name, value: false});
+		return writeList(key, todo);
+	});
+}
+
+function setItem(key, name, value) {
+	return readList(key).then(function(todo) {
+		for (var i = 0; i < todo.items.length; i++) {
+			if (todo.items[i].name == name) {
+				todo.items[i].value = value;
+			}
+		}
+		return writeList(key, todo);
+	});
+}
+
+function removeItem(key, name) {
+	return readList(key).then(function(todo) {
+		todo.items = todo.items.filter(function(item) {
+			return item.name != name;
+		});
+		return writeList(key, todo);
+	});
+}
+
+function printList(name, key, items) {
+	imports.terminal.print(name);
+	imports.terminal.print("=".repeat(name.length));
+	for (var i = 0; i < items.length; i++) {
+		var isChecked = items[i].value;
+		imports.terminal.print(
+			{command: "action:" + JSON.stringify({action: "set", key: key, item: items[i].name, value: !isChecked}), value: isChecked ? kChecked : kUnchecked},
+			" ",
+			items[i].name,
+			" (",
+			{command: "action:" + JSON.stringify({action: "remove", key: key, item: items[i].name}), value: "x"},
+			")");
+	}
+}
+
+function redisplay() {
+	imports.terminal.clear();
+	imports.terminal.setEcho(false);
+	var key = "todo";
+	readList(key).then(function(todo) {
+		printList(todo.name, key, todo.items);
+	}).catch(function(error) {
+		imports.terminal.print("ERROR: " + error);
+	});
+}
+
+redisplay();
