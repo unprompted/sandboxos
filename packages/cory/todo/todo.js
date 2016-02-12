19973891bcdf4db392b125277c0f1998407191f3
cory <cory@ed5197a5-7fde-0310-b194-c3ffbd925b24>
2016-02-12 14:18:21 +0000

    Multiple TODO list support.

--- packages/cory/todo/todo.js
+++ packages/cory/todo/todo.js
@@ -3,18 +3,46 @@
 var kUnchecked = "☐";
 var kChecked = "☑";
 
+let activeList = null;
+let confirmRemove;
+
 terminal.setPrompt("Add Item>");
 
 core.register("onInput", function(command) {
 	if (command.substring(0, "action:".length) == "action:") {
 		command = JSON.parse(command.substring("action:".length));
+		if (confirmRemove && command.action != "reallyRemoveList" && command.action != "reallyRemove") {
+			confirmRemove = false;
+		}
 		if (command.action == "set") {
 			setItem(command.key, command.item, command.value).then(notifyChanged).then(redisplay);
 		} else if (command.action == "remove") {
+			confirmRemove = command;
+			redisplay();
+		} else if (command.action == "reallyRemove") {
+			confirmRemove = false;
 			removeItem(command.key, command.item).then(notifyChanged).then(redisplay);
+		} else if (command.action == "editList") {
+			activeList = command.key;
+			redisplay();
+		} else if (command.action == "lists") {
+			activeList = null;
+			redisplay();
+		} else if (command.action == "removeList") {
+			confirmRemove = true;
+			redisplay();
+		} else if (command.action == "reallyRemoveList") {
+			confirmRemove = false;
+			activeList = null;
+			database.remove(command.key).then(notifyChanged).then(redisplay);
 		}
 	} else {
-		addItem("todo", command).then(notifyChanged).then(redisplay);
+		if (activeList) {
+			addItem(activeList, command).then(notifyChanged).then(redisplay);
+		} else {
+			activeList = makePrivateKey(command);
+			writeList(activeList, {name: command, items: []}).then(notifyChanged).then(redisplay);
+		}
 	}
 });
 
@@ -30,10 +58,12 @@ function readList(key) {
 	return database.get(key).catch(function(error) {
 		return null;
 	}).then(function(todo) {
-		if (!todo) {
-			todo = JSON.stringify({name: "TODO", items: []});
+		try {
+			todo = JSON.parse(todo);
+		} catch (error) {
+			todo = {name: "TODO", items: []};
 		}
-		return JSON.parse(todo);
+		return todo;
 	});
 }
 
@@ -69,7 +99,11 @@ function removeItem(key, name) {
 }
 
 function printList(name, key, items) {
-	terminal.print(name);
+	terminal.print(name,
+		" - ",
+		{command: "action:" + JSON.stringify({action: "lists"}), value: "back"},
+		" - ",
+		{command: "action:" + JSON.stringify({action: (confirmRemove === true ? "reallyRemoveList" : "removeList"), key: key}), value: (confirmRemove === true ? "confirm remove" : "remove")});
 	terminal.print("=".repeat(name.length));
 	for (var i = 0; i < items.length; i++) {
 		var isChecked = items[i].value;
@@ -79,7 +113,10 @@ function printList(name, key, items) {
 			" ",
 			{style: style[isChecked ? 1 : 0], value: items[i].name},
 			" (",
-			{command: "action:" + JSON.stringify({action: "remove", key: key, item: items[i].name}), value: "x"},
+			{command: "action:" + JSON.stringify({
+				action: (confirmRemove && confirmRemove.item == items[i].name ? "reallyRemove" : "remove"),
+				key: key,
+				item: items[i].name}), value: (confirmRemove && confirmRemove.item == items[i].name ? "confirm remove" : "remove")},
 			")");
 	}
 }
@@ -87,11 +124,70 @@ function printList(name, key, items) {
 function redisplay() {
 	terminal.clear();
 	terminal.setEcho(false);
-	var key = "todo";
-	readList(key).then(function(todo) {
-		printList(todo.name, key, todo.items);
-	}).catch(function(error) {
-		terminal.print("ERROR: " + error);
+	if (activeList) {
+		readList(activeList).then(function(data) {
+			printList(getName(activeList), activeList, data.items);
+		}).catch(function(error) {
+			terminal.print("error: " + error);
+		});
+	} else {
+		printListOfLists();
+	}
+}
+
+function makeId() {
+	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var result = "";
+	for (var i = 0; i < 32; i++) {
+		result += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+	}
+	return result;
+}
+
+function makePublicKey(name) {
+	return JSON.stringify({public: true, id: makeId(), name: name});
+}
+
+function makePrivateKey(name) {
+	return JSON.stringify({public: false, id: makeId(), name: name, user: core.user.name});
+}
+
+function hasPermission(key) {
+	let result = false;
+	try {
+		let data = JSON.parse(key);
+		result = data.public || data.user == core.user.name;
+	} catch (error) {
+		result = true;
+	}
+	return result;
+}
+
+function getName(key) {
+	let name = "TODO";
+	try {
+		name = JSON.parse(key).name || name;
+	} catch (error) {
+	}
+	return name;
+}
+
+function getVisibleLists() {
+	return database.getAll().then(function(data) {
+		return data.filter(hasPermission);
+	});
+}
+
+function printListOfLists() {
+	terminal.print("TODO Lists:");
+	getVisibleLists().then(function(keys) {
+		for (var i = 0; i < keys.length; i++) {
+			let key = keys[i];
+			terminal.print({
+				command: "action:" + JSON.stringify({action: "editList", key: key}),
+				value: getName(key),
+			});
+		}
 	});
 }
 
